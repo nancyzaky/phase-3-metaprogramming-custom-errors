307fae486fbb2e55efdcb885276e7697d54e6a01
Nancy Zaky <nancyzki@yahoo.com>
2021-09-02 18:49:17 -0700

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,20 +1,37 @@
+require"pry"
 class Person
   attr_accessor :partner, :name
+class PartnerError < StandardError
 
+def message
+ "you must give the get_married method an argument of an instance of the person class!"
+end
+end
   def initialize(name)
     @name = name
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
   end
+
+
 
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+# binding.pry
 
 
 
